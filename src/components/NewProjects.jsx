33876778a65ae62bd21677a275d8b4c1295dcd3f
jacovb <vbiljon@gmail.com>
2021-03-04 07:12:02 +0000

    some css, still a mess

--- src/components/NewProjects.jsx
+++ src/components/NewProjects.jsx
@@ -5,47 +5,51 @@ export default function NewProjects({formData, createProject, handleAddData}) {
     // needs to check if project number already exists
 
     return (
-        <>
-            <p>Add New Projects</p>
-            <label htmlFor="projNo">Project Number: </label>
-            <input 
-                type="text"
-                id="projNo"
-                value={formData.projectNo}
-                name="projectNo"
-                onChange={handleAddData}
-                required
-            />
-            <br/>
-            <label htmlFor="projName">Project Name: </label>
-            <input 
-                type="text"
-                id="projName"
-                value={formData.name}
-                name="name"
-                onChange={handleAddData}
-            />
-            <br/>
-            <label htmlFor="projHours">Allowed Hours: </label>
-            <input 
-                type="number"
-                id="projHours"
-                value={formData.allowedHours}
-                name="allowedHours"
-                onChange={handleAddData}
-            />
-            <br/>
-            <label htmlFor="status">Project Status: </label>
-            <select type="text"
-                id="status"
-                name="status"
-                onChange={handleAddData}>
-                    <option value="Quote">Quote</option>
-                    <option value="Current">Current</option>
-                    <option value="Complete">Complete</option>   
-            </select>
-            <br/>
+        <form className="label-to-bottom">
+            <h2>Add New Projects</h2>
+            <div>
+                <input 
+                    type="text"
+                    id="projNo"
+                    value={formData.projectNo}
+                    name="projectNo"
+                    onChange={handleAddData}
+                    required
+                />
+                <label htmlFor="projNo">Project Number: </label>
+            </div>
+            <div>
+                <input 
+                    type="text"
+                    id="projName"
+                    value={formData.name}
+                    name="name"
+                    onChange={handleAddData}
+                />
+                <label htmlFor="projName">Project Name: </label>
+            </div>
+            <div>
+                <input 
+                    type="number"
+                    id="projHours"
+                    value={formData.allowedHours}
+                    name="allowedHours"
+                    onChange={handleAddData}
+                />
+                <label htmlFor="projHours">Allowed Hours: </label>
+            </div>
+            <div>
+                <select type="text"
+                    id="status"
+                    name="status"
+                    onChange={handleAddData}>
+                        <option value="Quote">Quote</option>
+                        <option value="Current">Current</option>
+                        <option value="Complete">Complete</option>   
+                </select>
+                <label htmlFor="status">Project Status: </label>
+            </div>
             <button onClick={createProject}>Add New Project</button>
-        </>
+        </form>
     )
 }
