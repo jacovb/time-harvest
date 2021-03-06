3a76a72bc589b2d181e569878173ce6513c45744
jacovb <vbiljon@gmail.com>
2021-03-06 13:22:20 +0000

    css magic added from dev ed, need to customise for app

--- src/components/NewProjects.jsx
+++ src/components/NewProjects.jsx
@@ -7,28 +7,34 @@ export default function NewProjects({formData, createProject, handleAddData}) {
     return (
         <form className="label-to-bottom">
             <h2>Add New Projects</h2>
-            <div>
+            <div className="projNo-section">
                 <input 
                     type="text"
                     id="projNo"
                     value={formData.projectNo}
                     name="projectNo"
                     onChange={handleAddData}
+                    autoComplete="off"
                     required
                 />
-                <label htmlFor="projNo">Project Number: </label>
+                <label htmlFor="projNo" className="label-projNo">
+                    <span className="content-name">Project Number:</span>
+                </label>
             </div>
-            <div>
+            <div className="projName-section">
                 <input 
                     type="text"
                     id="projName"
                     value={formData.name}
                     name="name"
                     onChange={handleAddData}
+                    autoComplete="off"
                 />
-                <label htmlFor="projName">Project Name: </label>
+                <label htmlFor="projName" className="label-projName">
+                    <span className="content-name">Project Name:</span> 
+                </label>
             </div>
-            <div>
+            <div className="projHours-section">
                 <input 
                     type="number"
                     id="projHours"
@@ -36,9 +42,11 @@ export default function NewProjects({formData, createProject, handleAddData}) {
                     name="allowedHours"
                     onChange={handleAddData}
                 />
-                <label htmlFor="projHours">Allowed Hours: </label>
+                <label htmlFor="projHours" className="label-projHours">
+                    <span className="content-name">Allowed Hours:</span>
+                </label>
             </div>
-            <div>
+            <div className="projStatus-section">
                 <select type="text"
                     id="status"
                     name="status"
@@ -47,7 +55,9 @@ export default function NewProjects({formData, createProject, handleAddData}) {
                         <option value="Current">Current</option>
                         <option value="Complete">Complete</option>   
                 </select>
-                <label htmlFor="status">Project Status: </label>
+                <label htmlFor="status" className="label-projStatus">
+                    <span className="content-name">Project Status:</span>
+                </label>
             </div>
             <button onClick={createProject}>Add New Project</button>
         </form>
