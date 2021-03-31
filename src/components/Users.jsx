a2180e71c7b63b5e2214ee6149119caa064ebed3
jacovb <vbiljon@gmail.com>
2021-03-31 07:12:03 +0100

    added dept to Users and CSS User form

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -9,33 +9,55 @@ export default function Users({
   deleteUser
 }) {
     return (
-        <>
-          <p>Add New Users</p>
-          <label htmlFor="userField">User Name: </label>
-          <input 
-              type="text"
-              id="userField"
-              value={userData.name}
-              name="name"
-              onChange={handleAddUser}
-              required
-          />
-          <br/>
+      <>
+        <div className="userHeading">
+          <h2>Add New Users</h2>
+        </div>
+        <div className="project-form">
+          <div className="fullwidth-input">
+            <label htmlFor="userField">User Name: </label>
+            <input 
+                type="text"
+                id="userField"
+                value={userData.name}
+                name="name"
+                onChange={handleAddUser}
+                required
+            />
+          </div>
+          
+          <div className="fullwidth-input">
+            <label htmlFor="addDepartment">Department: </label>
+            <select 
+                type="text"
+                id="addDepartment"
+                name="department"
+                value={userData.department}
+                required
+                onChange={handleAddUser}>
+                    <option value="" disabled hidden>-- Select Department --</option>
+                    <option value="Coordination">Coordination</option>
+                    <option value="Technical">Technical</option>
+                    <option value="Engineering">Engineering</option>
+                    <option value="Construction">Construction</option>   
+            </select>
+          </div>
           <button onClick={createUser}>Add New User</button>
-          <br/>
-          <div>
-              {users
-                .map((user, idx) => (
-                  <div key={idx}>
-                    <div>{user.name}</div>
-                    <button
-                      onClick={() => deleteUser(user)}
-                    >
-                      <HighlightOffIcon />
-                    </button>
-                  </div>
-                ))}
-            </div>
-        </>
+        </div>
+        <div className="userList">
+            {users
+              .map((user, idx) => (
+                <div className="userRow" key={idx}>
+                  <div>{user.name}</div>
+                  <div>{user.department}</div>
+                  <button
+                    onClick={() => deleteUser(user)}
+                  >
+                    <HighlightOffIcon />
+                  </button>
+                </div>
+              ))}
+          </div>
+      </>
     )
 }
