3333a85f23707e61a63b6d629c6e4da7ec6ab296
jacovb <vbiljon@gmail.com>
2021-04-05 09:29:59 +0100

    small updates

--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -7,33 +7,37 @@ export default function Reports({
 }) {
     return (
         <>
-          <h2>Reports</h2>
+          <div className="projListHeading">
+            <h2>Reports</h2>
+          </div>
           <div className="projectsList">
-            {projects.map((project, idx) => (
-              <div className="projectRow" key={idx}>
-                <div className="gridProjectNo">{project.projectNo}</div>
-                  <div className="gridProjectName">{project.name}</div>
-                  <div className="gridProjectStatus">{project.status}</div>
-                  <ProgressRing 
-                    className="gridProgress-ring"
-                    usedHours={project.usedTimeTechnical}
-                    allowedHours={project.allowTimeTechnical}
-                    />
-                  <ProgressRing 
-                    className="gridProgress-ring"
-                    usedHours={project.usedTimeCoordination}
-                    allowedHours={project.allowTimeCoordination}
-                    />
-                  <ProgressRing 
-                    className="gridProgress-ring"
-                    usedHours={project.usedTimeEngineering}
-                    allowedHours={project.allowTimeEngineering}
-                    />
-                  <ProgressRing 
-                    className="gridProgress-ring"
-                    usedHours={project.usedTimeConstruction}
-                    allowedHours={project.allowTimeConstruction}
-                    />
+            {projects
+              .sort((a, b) => a.projectNo - b.projectNo)
+              .map((project, idx) => (
+                <div className="projectRow" key={idx}>
+                  <div className="gridProjectNo">{project.projectNo}</div>
+                    <div className="gridProjectName">{project.name}</div>
+                    <div className="gridProjectStatus">{project.status}</div>
+                    <ProgressRing 
+                      className="gridProgress-ring"
+                      usedHours={project.usedTimeTechnical}
+                      allowedHours={project.allowTimeTechnical}
+                      />
+                    <ProgressRing 
+                      className="gridProgress-ring"
+                      usedHours={project.usedTimeCoordination}
+                      allowedHours={project.allowTimeCoordination}
+                      />
+                    <ProgressRing 
+                      className="gridProgress-ring"
+                      usedHours={project.usedTimeEngineering}
+                      allowedHours={project.allowTimeEngineering}
+                      />
+                    <ProgressRing 
+                      className="gridProgress-ring"
+                      usedHours={project.usedTimeConstruction}
+                      allowedHours={project.allowTimeConstruction}
+                      />
               </div>
             ))}
           </div>
