e2c1cd6e58e79e243375b011aa2165de5d8bd899
jacovb <vbiljon@gmail.com>
2021-04-17 10:35:38 +0100

    dates - fun

--- src/components/MonthlyHoursBreakdown.jsx
+++ src/components/MonthlyHoursBreakdown.jsx
@@ -5,19 +5,30 @@ export default function MonthlyHoursBreakdown({
   users,
   entry,
 }) {
+    
+    const entryDates = 
+    users.map((user) => {
+      return entry
+        .filter((item) => item.user.id === user.id)
+        .sort((a, b) => new Date(b.date) - new Date(a.date))
+        .map((item, idx) => new Date(item.date).toLocaleString('default', { month: 'long' }) + " " + new Date(item.date).getFullYear())
+        .reduce((acc, curr) => acc.includes(curr) ? acc : [...acc, curr], []);
+    })
+    
     return (
         <>
           {console.log("Projects", projects)}
           {console.log("Entries", entry)}
           {console.log("Users", users)}
+          {console.log("Entry Dates",entryDates)}
 
           <div className="projListHeading">
             <h2>Hours per Person</h2>
           </div>
 
           <div className="table-person">
-            {users.map((user) => (
-              <table>
+            {users.map((user, idx) => (
+              <table key={idx}>
                 <thead>
                   <h2>{user.name}</h2>
                   <tr>
@@ -30,9 +41,9 @@ export default function MonthlyHoursBreakdown({
                 </thead>
                 {entry
                   .filter((obj) => obj.user.id === user.id)
-                  .sort((a, b) => new Date(a.date) - new Date(b.date))
-                  .map((obj) => (
-                  <tbody>
+                  .sort((a, b) => new Date(b.date) - new Date(a.date))
+                  .map((obj, idx) => (
+                  <tbody key={idx}>
                     <tr>
                       <td>{obj.date}</td>
                       <td>{obj.project.projectNo}</td>
