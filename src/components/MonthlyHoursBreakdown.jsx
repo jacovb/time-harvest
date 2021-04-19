100e8c80ef2b9cbda2be3109b49f0c63a21beb62
jacovb <vbiljon@gmail.com>
2021-04-19 19:43:38 +0100

    small css adjustments

--- src/components/MonthlyHoursBreakdown.jsx
+++ src/components/MonthlyHoursBreakdown.jsx
@@ -25,14 +25,16 @@ export default function MonthlyHoursBreakdown({
           {/* Map through users, map through entryDates(months), 
           then map through all entries and use Users and entryDates to group */}
           <div className="table-person">
+            <div className="table-heading">
+              <div className="c-1 thead">Date</div>
+              <div className="c-2 thead">Project No.</div>
+              <div className="c-3 thead">Name</div>
+              <div className="c-4 thead">Description</div>
+              <div className="c-5 thead">Time</div>
+            </div>
             {users.map((user, idx) => (
               <div className="table" key={idx}>
                 <h2>{user.name}</h2>
-                <div className="c-1 thead">Date</div>
-                <div className="c-2 thead">Project No.</div>
-                <div className="c-3 thead">Name</div>
-                <div className="c-4 thead">Description</div>
-                <div className="c-5 thead">Time</div>
                 {entryDates[idx].map((month, idx) => (
                   <React.Fragment key={idx}>
                     <h5 className="c-1">{month}</h5>
@@ -49,7 +51,7 @@ export default function MonthlyHoursBreakdown({
                         <div className="c-5">{obj.time}</div>
                       </React.Fragment>
                     ))}
-                    <div className="c-5">
+                    <div className="c-5 total">
                       {entry
                         .filter((obj) => obj.user.id === user.id)
                         .filter((obj) => new Date(obj.date).toLocaleString('default', { month: 'long' }) === month.split(" ")[0])
