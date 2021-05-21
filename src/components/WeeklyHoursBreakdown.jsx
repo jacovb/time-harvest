c9bc8bfe50872c142257b81c3aecbb8259c7f9f3
jacovb <vbiljon@gmail.com>
2021-05-21 07:04:48 +0100

    grouping entries by week

--- src/components/WeeklyHoursBreakdown.jsx
+++ src/components/WeeklyHoursBreakdown.jsx
@@ -14,28 +14,59 @@ export default function WeeklyHoursBreakdown() {
     return [d.getFullYear(), weekNo]
   }
 
-  function getFirstAndLastDayOfWeek(day) {
-    let d = new Date(day);
-    let first = d.getDate() - d.getDay();
-    let last = first + 6;
+  // function getFirstAndLastDayOfWeek(day) {
+  //   let d = new Date(day);
+  //   let first = d.getDate() - d.getDay();
+  //   let last = first + 6;
   
-    let firstday = new Date(d.setDate(first)).toISOString().split('T')[0];
-    let lastday = new Date(d.setDate(last)).toISOString().split('T')[0];
-    return [firstday, lastday];
+  //   let firstday = new Date(d.setDate(first)).toISOString().split('T')[0];
+  //   let lastday = new Date(d.setDate(last)).toISOString().split('T')[0];
+  //   return [firstday, lastday];
+  // }
+
+  function getFirstAndLastDayOfWeek(week) {
+    let year = week[0];
+    let weekNo = week[1];
+    let d = new Date("Jan 01, " + year + " 01:00:00");
+    let w = d.getTime() + 604800000 * (weekNo - 1); // 1 week in milliseconds
+    let firstday = new Date(w);
+    let lastday = new Date(w + 518400000) // 6 days in milliseconds
+    return [firstday.toISOString().split('T')[0], lastday.toISOString().split('T')[0]] 
   }
+
+  const entryDates = 
+    context.users.map((user) => {
+      return context.entry
+        .filter((item) => item.user.id === user.id)
+        .sort((a, b) => new Date(a.date) - new Date(b.date))
+        .map((item, idx) => new Date(item.date)
+          .toISOString().split('T')[0])
+        .reduce((acc, curr) => acc.includes(curr) ? acc : [...acc, curr], [])
+    })
+
+  const entryDateArray = 
+    entryDates
+      .reduce((a, b) => {
+          return a.concat(b)
+        }, [])
+      .reduce((acc, curr) => acc.includes(curr) ? acc : [...acc, curr], [])
+
+  const weekNumbersRaw = entryDateArray
+    .map((date, idx) => getWeekNumber(date))
+    .reduce((acc, curr) => acc.includes(curr) ? acc : [...acc, curr], []);
   
-  let date = "2021-03-24"
-  let weekdays = getFirstAndLastDayOfWeek(date)
-  let week = getWeekNumber(date);
-  console.log(date + " is in week" + week[1] + ' of ' + week[0]);
-  console.log("First Day of that week is " + weekdays[0] + ", and last day of that week is " + weekdays[1]);
-  console.log(context);
+  const weekNumbersArray = Array.from(new Set(weekNumbersRaw.map(JSON.stringify)), JSON.parse)
   
-    return (
-        <>
-          <div className="projListHeading">
-            <h2>Weekly Hours per Person</h2>
-          </div>
-        </>
-    )
+  console.log(entryDates);
+  console.log(entryDateArray);
+  console.log(weekNumbersArray.sort((a, b) => a[0] - b[0]).sort((a, b) => a[1] - b[1]));
+  console.log(getFirstAndLastDayOfWeek([2021, 11]));
+  
+  return (
+      <>
+        <div className="projListHeading">
+          <h2>Weekly Hours per Person</h2>
+        </div>
+      </>
+  )
 }
