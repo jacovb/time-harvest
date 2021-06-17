b1afabf97272e40d888476445ad121d52a1d2708
jacovb <vbiljon@gmail.com>
2021-06-17 06:57:21 +0100

    year date slider works, entries need to be filtered accordingly

--- src/components/ProjectDistribution.jsx
+++ src/components/ProjectDistribution.jsx
@@ -1,6 +1,9 @@
 import React, { useContext, useState } from 'react';
 import { RenderContext } from "../context/RenderContext";
 
+import FastRewindIcon from '@material-ui/icons/FastRewind';
+import FastForwardIcon from '@material-ui/icons/FastForward';
+
 export default function ProjectDistribution() {
   const context = useContext(RenderContext);
   const [day, setDay] = useState(new Date());
@@ -8,14 +11,16 @@ export default function ProjectDistribution() {
   function getLast12Months(d) {
     const monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     let monthArray = [];
-    d.setDate(1);
+    let currentDate = new Date(d)
+    currentDate.setDate(1);
     for (let i = 0; i <= 11; i++) {
-      monthArray.push(monthName[d.getMonth()] + " " + d.getFullYear());
-      d.setMonth(d.getMonth() - 1)
+      monthArray.push(monthName[currentDate.getMonth()] + " " + currentDate.getFullYear());
+      currentDate.setMonth(currentDate.getMonth() - 1)
     }
     return monthArray.reverse();
   }
 
+  // add slider buttons - onClick, setDay to a year ago / ahead
   const months = getLast12Months(day);
 
   return (
@@ -25,10 +30,36 @@ export default function ProjectDistribution() {
       </div>
       
       <div className={"dist-table"}>
+        <button 
+          className="year-back d-1 year-slider"
+          onClick={() => {
+            let tempDay = new Date(day);
+            let lastYearDay = tempDay.setMonth(tempDay.getMonth() - 12);
+            setDay(lastYearDay)
+          }}
+        >
+          <FastRewindIcon/>
+        </button>
+
+        <div className="dist-date-range">{`${months[0]} - ${months[11]}`}</div>
+
+        <button 
+            className="year-forward d-14 year-slider"
+            onClick={() => {
+              let tempDay = new Date(day);
+              let nextYearDay = tempDay.setMonth(tempDay.getMonth() + 12);
+              setDay(nextYearDay)
+            }}
+          >
+            <FastForwardIcon/>
+        </button>
+
         {months.map((month, idx) => (
             <div key={idx} className={`d-${idx+2} dist-headings`}>{month}</div>
         ))}
-        <div className="dist-headings dist-strong">TOTAL</div>
+        <div className="dist-headings dist-strong">
+          TOTAL
+        </div>
         {context.projects
           .sort((a, b) => a.projectNo - b.projectNo)
           .map((project, idx) => (
