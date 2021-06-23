24703f644094790e0d0c2faf71fb264cb4bb9cd1
jacovb <vbiljon@gmail.com>
2021-06-23 20:48:28 +0100

    codebar sesh

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import Reports from "./components/Reports";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders Welcome link", () => {
+  render(<Reports />);
+  const linkElement = screen.getByText(/Reports/i);
   expect(linkElement).toBeInTheDocument();
 });
