119fee1ce6a18e8d19405943af0964ce273559a0
jacovb <vbiljon@gmail.com>
2021-07-01 06:51:40 +0100

    d3 axis

--- src/components/ProjectDistributionGraph.jsx
+++ src/components/ProjectDistributionGraph.jsx
@@ -4,18 +4,43 @@ import * as d3 from 'd3'
 export default function ProjectDistributionGraph() {
   const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75])
   const svgRef = useRef();
-  console.log(svgRef)
+  const svgStyle = {
+    overflow: "visible",
+    transform: "translateX(150px)",
+  }
 
   useEffect(() => {
     const canvas = d3.select(svgRef.current)
+    const xScale = d3.scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, 300])
+
+    const yScale = d3.scaleLinear()
+      .domain([0, 100])
+      .range([150, 0])
+
+    const xAxis = d3.axisBottom(xScale).ticks(data.length)
+
+    canvas
+      .select(".x-axis")
+      .style("transform", "translateY(150px)")
+      .call(xAxis)
+
+    const yAxis = d3.axisLeft(yScale)
+    
+    canvas
+      .select(".y-axis")
+      .call(yAxis)
+    
     const myLine = d3.line()
-      .x((value, index) => index * 50)
-      .y(value => 150 - value)
+      .x((value, index) => xScale(index))
+      .y(value => yScale(value))
       .curve(d3.curveCardinal)
     canvas
-      .selectAll("path")
+      .selectAll(".line")
       .data([data])
       .join("path")
+      .attr("class", "line")
       .attr("d", value => myLine(value))
       .attr("fill", "none")
       .attr("stroke", "blue")
@@ -24,15 +49,18 @@ export default function ProjectDistributionGraph() {
       .data(data)
       .join("circle")
       .attr("r", 5)
-      .attr("cx", (value, index) => index * 50)
-      .attr("cy", value => 150 - value)
+      .attr("cx", (value, index) => xScale(index))
+      .attr("cy", value => yScale(value))
       .attr("stroke", "black")
       .attr("fill", "white")
   }, [data])
 
   return (
     <>
-      <svg ref={svgRef} />
+      <svg ref={svgRef} style={svgStyle}> 
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
       {/* <br />
       <button onClick={() => setData(data.map(value => value + 5))}>
         Update Data
