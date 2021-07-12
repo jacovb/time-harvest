de95ce5745665b29c21b748b39354597ffc5733c
jacovb <vbiljon@gmail.com>
2021-07-12 06:57:02 +0100

    added styling to chart - needs auto-update

--- src/components/ProjectDistributionChart.jsx
+++ src/components/ProjectDistributionChart.jsx
@@ -1,6 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Chart from 'chart.js/auto';
 
+const colors = [
+  "#006ba6",
+  "#0496ff",
+  "#ffbc42",
+  "#ff686b",
+  "#ffa69e",
+  "#3c787e",
+]
+
 export default function ProjectDistricutionChart ({
   months,
   projects,
@@ -10,7 +19,10 @@ export default function ProjectDistricutionChart ({
   let dataForChart = projects.map((project, idx) => (
     { 
       label: `${projects[idx].projectNo} - ${projects[idx].name}`,
-      data: entries[idx]
+      data: entries[idx],
+      borderColor: colors[idx],
+      backgroundColor: `${colors[idx]}33`, // add 33 (alpha-transparence) to hex color for 20% opacity
+      tension: 0.2,
     }
   ))
 
@@ -19,6 +31,18 @@ export default function ProjectDistricutionChart ({
     data: {
       labels: months,
       datasets: dataForChart
+    },
+    options: {
+      plugins: {
+          title: {
+              display: true,
+              text: 'Monthly Hours per Project',
+              padding: {
+                top: 0,
+                bottom: 10,
+              }
+          }
+      }
     }
   }
 
